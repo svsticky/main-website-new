463bd8324f50d4120ffda8575a78363984535a7e
Job Vonk <sticcpistt@icloud.com>
2026-08-05 23:21:55 +0200

    Added sitemap generation build step

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import preload from "astro-preload";
 import playformCompress from "@playform/compress";
 import favicons from "astro-favicons";
 
+import sitemap from "@astrojs/sitemap";
+
 const env = loadEnv(import.meta.env.MODE, process.cwd(), '');
 const {
   STORYBLOK_DELIVERY_API_TOKEN,
@@ -24,73 +26,67 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [
-    storyblok({
-      accessToken: STORYBLOK_DELIVERY_API_TOKEN,
-      apiOptions: {
-        region: STORYBLOK_REGION || 'eu',
-      },
-      components: {
-        "page": 'components/Page',
-        "grid": 'components/basics/Grid',
-        "card": "components/basics/Card",
-        "rich-text": "components/basics/RichText",
-        "image": "components/basics/Image",
-        "hero": "components/home/Hero",
-        "button": "components/basics/Button",
-        "board": "components/board/Board",
-        "partner": "components/partners/Partner",
-        "partner-overview": "components/partners/Overview",
-        "vacancy": "components/vacancies/Vacancy",
-        "vacancy-overview": "components/vacancies/Overview",
-        "activities": "components/Activities",
-        "committee": "components/committees/Committee",
-        "committee-overview": "components/committees/Overview",
-        "society-overview": "components/societies/Overview",
-        "society": "components/societies/Society",
-        "vcp-page": "components/Vcp",
-        "lvv-page": "components/Lvv",
-        "contact-grid": "components/Contact",
-        "links-page": "components/Links",
-        "timeline": "components/Timeline",
-        "stats-section": "components/home/StatsSection",
-        "featured-partner-banner": "components/home/FeaturedPartnerBanner",
-        "info-columns": "components/home/InfoColumns",
-        "activities-carousel": "components/home/ActivitiesCarousel",
-        "news-carousel": "components/home/NewsCarousel",
-        "news-item": "components/news/News",
-        "news-overview": "components/news/Overview",
-        "separator": "components/basics/Separator",
-        "title": "components/basics/Title",
-        "current-board": "components/board/Current",
-        "old-boards": "components/board/Old",
-        "button-grid": "components/basics/ButtonGrid",
-        "branding-colour": "components/branding/Colour",
-        "branding-logos": "components/branding/Logos",
-        "branding-poster": "components/branding/PosterSpecs",
-        "branding-twocol": "components/branding/TwoColumn",
-        "partner_carousel": "components/collaboration/PartnerCarousel",
-        "promotion": "components/collaboration/Promotion",
-        "partnering_reasons": "components/collaboration/PartneringReasons",
-        "collaboration-form": "components/collaboration/ContactForm"
-      },
-    }),
-    react(),
-    preload(),
-    playformCompress({ CSS: false, Path: ["./dist/client", "./dist/assets"] }),
-    favicons({
-      input: await fetch("https://public.svsticky.nl/logos/hoofd_outline_kleur.svg")
-        .then(resp => resp.text())
-        .then(Buffer.from),
-      name: "Studievereniging Sticky",
-      short_name: "Studievereniging Sticky",
-      name_localized: {
-        "nl": "Studievereniging Sticky",
-        "en": "Study Association Sticky"
-      },
-      themes: ["#fa6b20"]
-    })
-  ],
+  integrations: [storyblok({
+    accessToken: STORYBLOK_DELIVERY_API_TOKEN,
+    apiOptions: {
+      region: STORYBLOK_REGION || 'eu',
+    },
+    components: {
+      "page": 'components/Page',
+      "grid": 'components/basics/Grid',
+      "card": "components/basics/Card",
+      "rich-text": "components/basics/RichText",
+      "image": "components/basics/Image",
+      "hero": "components/home/Hero",
+      "button": "components/basics/Button",
+      "board": "components/board/Board",
+      "partner": "components/partners/Partner",
+      "partner-overview": "components/partners/Overview",
+      "vacancy": "components/vacancies/Vacancy",
+      "vacancy-overview": "components/vacancies/Overview",
+      "activities": "components/Activities",
+      "committee": "components/committees/Committee",
+      "committee-overview": "components/committees/Overview",
+      "society-overview": "components/societies/Overview",
+      "society": "components/societies/Society",
+      "vcp-page": "components/Vcp",
+      "lvv-page": "components/Lvv",
+      "contact-grid": "components/Contact",
+      "links-page": "components/Links",
+      "timeline": "components/Timeline",
+      "stats-section": "components/home/StatsSection",
+      "featured-partner-banner": "components/home/FeaturedPartnerBanner",
+      "info-columns": "components/home/InfoColumns",
+      "activities-carousel": "components/home/ActivitiesCarousel",
+      "news-carousel": "components/home/NewsCarousel",
+      "news-item": "components/news/News",
+      "news-overview": "components/news/Overview",
+      "separator": "components/basics/Separator",
+      "title": "components/basics/Title",
+      "current-board": "components/board/Current",
+      "old-boards": "components/board/Old",
+      "button-grid": "components/basics/ButtonGrid",
+      "branding-colour": "components/branding/Colour",
+      "branding-logos": "components/branding/Logos",
+      "branding-poster": "components/branding/PosterSpecs",
+      "branding-twocol": "components/branding/TwoColumn",
+      "partner_carousel": "components/collaboration/PartnerCarousel",
+      "promotion": "components/collaboration/Promotion",
+      "partnering_reasons": "components/collaboration/PartneringReasons",
+      "collaboration-form": "components/collaboration/ContactForm"
+    },
+  }), react(), preload(), playformCompress({ CSS: false, Path: ["./dist/client", "./dist/assets"] }), favicons({
+    input: await fetch("https://public.svsticky.nl/logos/hoofd_outline_kleur.svg")
+      .then(resp => resp.text())
+      .then(Buffer.from),
+    name: "Studievereniging Sticky",
+    short_name: "Studievereniging Sticky",
+    name_localized: {
+      "nl": "Studievereniging Sticky",
+      "en": "Study Association Sticky"
+    },
+    themes: ["#fa6b20"]
+  }), sitemap()],
   vite: {
     plugins: [mkcert(), tailwindcss()],
     ssr: import.meta.env.MODE === "development" ? {} : {
